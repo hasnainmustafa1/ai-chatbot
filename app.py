6b4820cffac9217a3d0de64d1c01cd603cfb8076
Muhammad Hasnain Mustafa <hasnainmustafa7863@gmail.com>
2025-12-23 14:19:42 +0500

    app.py

--- app.py
+++ app.py
@@ -1,24 +1,20 @@
 import streamlit as st
 from google import genai
 
-st.set_page_config(page_title="AI Chatbot (Gemini)")
+st.set_page_config(page_title="🤖 AI Chatbot (Gemini)")
 
 st.title("🤖 AI Chatbot (Gemini)")
 user_input = st.text_input("Apna sawal likho aur AI se jawab lo")
 
-# Gemini client
 client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
 
 if user_input:
     try:
-        response = client.models.generate_content(
-            model="gemini-1.5-flash-001",
+        response = client.generate_content(
+            model="gemini-1.5-flash",
             contents=user_input
         )
-
         st.success(response.text)
-
     except Exception as e:
         st.error("Gemini API error aaya 😥")
-        st.code(str(e))
-
+        st.exception(e)
